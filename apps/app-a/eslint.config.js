bbf7fdb6ab02905b6005e918a5e5ef9ccb1cdc67
Luke Davis <luke@finesse.host>
2026-03-20 21:07:04 -0500

    reproduction

---
+++ apps/app-a/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "tooling-config/eslint";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default config;
---
+++ packages/tooling-config/eslint.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import turboPlugin from "eslint-plugin-turbo";
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const config = [
+  js.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+];
